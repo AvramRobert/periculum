2260e8ae8e8c9133c3b08d58c81f90e596d00f7b
Robert Marius Avram <am11.robert@yahoo.com>
2016-05-17 22:30:02 +0200

    Bits and pieces

--- src-common/periculum/core.clj
+++ src-common/periculum/core.clj
@@ -7,65 +7,84 @@
             [periculum.world :refer [make-world m-struct pos]]
             [clojure.core.match :refer [match]]))
 
-(defn derive-pos
-  [entity]
-  (let [x (get-in entity [:position :x])
-        y (get-in entity [:position :y])]
-    (block-pos x y)))
-
 (defn wall-shape [entity]
   (let [pos (derive-pos entity)]
     (assoc (shape :filled
-                  :set-color (color :red)
+                  :set-color (color :coral)
                   :rect 0 0 block-size block-size) :x (:x pos) :y (:y pos))))
+
+(defn wall-texture [entity]
+  (let [pos (derive-pos entity)]
+    (assoc (texture "brick.png")
+      :x (:x pos) :y (:y pos) :width block-size :height block-size)))
 
 (defn floor-shape [entity]
   (let [pos (derive-pos entity)]
     (assoc (shape :filled
-                  :set-color (color :green)
+                  :set-color (color :gray)
                   :rect 0 0 block-size block-size) :x (:x pos) :y (:y pos))))
 
-(defn player-shape [x y]
+(defn floor-texture [entity]
+  (let [pos (derive-pos entity)]
+    (assoc (texture "grass_side.png")
+      :x (:x pos) :y (:y pos) :width block-size :height block-size)))
+
+(defn agent-shape [x y]
   (let [pos (block-pos x y)]
     (assoc (shape :filled
-                  :set-color (color :white)
-                  :rect 0 0 block-size block-size) :x (:x pos) :y (:y pos))))
+                  :set-color (color :scarlet)
+                  :circle 0 0 half-block) :x (+ (:x pos) half-block) :y (+ (:y pos) half-block))))
+
+(defn agent-texture [x y]
+  (let [pos (block-pos x y)]
+    (assoc (texture "krabby.png")
+      :x (:x pos) :y (:y pos) :width block-size :height block-size)))
 
 (defn pause [amount entities]
   (Thread/sleep amount)
   entities)
 
-(defn enlighten [entities]
-  (let [ne (map
-             (fn [e]
-               (let [x (:x e)
-                     y (:y e)]
-                 (assoc (shape e
-                               :set-color (color! (shape! e :get-color) :sub 0.015 0.015 0.0 0.0)
-                               :rect 0 0 block-size block-size) :x x :y y)))
-             entities)]
-    ne))
+(def resize
+  (fn [screen entities]
+    (height! screen 600)))
 
-(defscreen main-screen
+(declare periculum-game sprite-screen simple-screen value-screen)
+
+(defscreen value-screen
+           :on-show
+           (fn [screen entities]
+             (update! screen :renderer (stage) :camera (orthographic)))
+
+           :on-resize resize
+
+           :on-render
+           (fn [screen entities]
+             (clear!)
+             (->> entities
+                  (show-qs)
+                  (render! screen))))
+
+
+(defscreen sprite-screen
            :on-show
            (fn [screen entities]
              (update! screen :renderer (stage) :camera (orthographic))
              (let [env (make-world world-config)
+                   landscape (assoc (texture "wall1.jpg")
+                               :width (game :width) :height (game :height))
                    walls (->> env
                               (filter #(= (:type %) :wall))
-                              (map wall-shape))
+                              (map wall-texture))
                    floors (->> env
                                (filter #(= (:type %) :floor))
-                               (map floor-shape))
-                   player (player-shape 0 1)]
-               (flatten [
+                               (map floor-texture))
+                   player (assoc (agent-texture 1 1) :e-type :texture)]
+               (flatten [[landscape]
                          (map #(assoc % :wall? true) walls)
                          (map #(assoc % :floor? true) floors)
                          [(assoc player :player? true)]])))
 
-           :on-resize
-           (fn [screen entities]
-             (height! screen 600))
+           :on-resize resize
 
            :on-render
            (fn [screen entities]
@@ -75,7 +94,42 @@
                   (show-choice)
                   (render! screen))))
 
+;:on-key-down
+;(fn [screen entities]
+;  (cond
+;    (= (:key screen) (key-code :dpad-left))
+;    (set-screen! @(resolve 'periculum.core/periculum-game)
+;                 @(resolve 'periculum.core/simple-screen))))
+
+(defscreen simple-screen
+           :on-show
+           (fn [screen entities]
+             (update! screen :renderer (stage) :camera (orthographic) :color :blue)
+             (let [env (make-world world-config)
+                   walls (->> env
+                              (filter #(= (:type %) :wall))
+                              (map wall-shape))
+                   floors (->> env
+                               (filter #(= (:type %) :floor))
+                               (map floor-shape))
+                   player (assoc (agent-shape 1 1) :e-type :geometric)]
+               (flatten [
+                         (map #(assoc % :wall? true) walls)
+                         (map #(assoc % :floor? true) floors)
+                         [(assoc player :player? true)]])))
+
+           :on-resize resize
+
+           :on-render
+           (fn [screen entities]
+             (clear!)
+             (->> entities
+                  ;(pause 50)
+                  (show-choice)
+                  (render! screen))))
+
+
 (defgame periculum-game
          :on-create
          (fn [this]
-           (set-screen! this main-screen)))
+           (set-screen! this simple-screen)))
--- src-common/periculum/prepare.clj
+++ src-common/periculum/prepare.clj
@@ -5,30 +5,37 @@
     [periculum.learning]
     [periculum.more])
   (:require
-    [clojure.core.async :as async]))
+    [clojure.core.async :as async]
+    [play-clj.core :refer [shape, color, color!]]))
 
-(def block-size 32)
+(def block-size 64)
+(def half-block (/ block-size 2))
+(def worst-bound -1000.0)
+(def best-bound 0.0)
 
-(def policy-channel (async/chan))
-(def qs-channel (async/chan))
+(def policy-channel (async/chan (async/buffer 2048)))
+(def qs-channel (async/chan (async/buffer 2048)))
 
 (defn state [x y action]
   (->State (->Pos x y) action))
 
-;(def world-config {:floor     (m-struct 11 0 (pos 0 0))
-;                 :holes     [(pos 4 0) (pos 5 0)]
-;                 :walls     [(m-struct 2 3 (pos 2 1))]
-;                 :platforms [(m-struct 3 (pos 4 5))]})
+(def world-config {:floor     (m-struct 16 0 (pos 0 0))
+                   :holes     [(pos 3 0) (pos 4 0) (pos 9 0) (pos 10 0)]
+                   :walls     [(m-struct 2 3 (pos 7 1))]
+                   :platforms [(m-struct 2 (pos 10 5))]})
 
-(def world-config {:floor     (m-struct 15 0 (pos 0 0))
-                   :holes     [(pos 3 0) (pos 13 0) (pos 14 0) (pos 15 0)]
-                   :walls     [(m-struct 2 3 (pos 5 1)) (m-struct 2 3 (pos 10 1))]
-                   :platforms empty-vec
-                   })
+;(def world-config {:floor     (m-struct 14 0 (pos 0 0))
+;                   :holes     [(pos 3 0) (pos 13 0) (pos 14 0) (pos 15 0)]
+;                   :walls     [(m-struct 2 3 (pos 5 1)) (m-struct 2 3 (pos 10 1))]
+;                   :platforms empty-vec
+;                   })
 
 (def world (make-world world-config))
 
-(defn default [algorithm]
+(defn t [value]
+  (Math/abs ^Double (/ value worst-bound)))
+
+(defn choice-supplier [algorithm]
   (let [data (conf 1.0 0.4 0.7)
         policy (eps-greedy 0.6 greedy-by-max policy-channel)
         terminal-f (terminal? world)
@@ -38,14 +45,15 @@
         exp-algorithm (algorithm policy action-f reward-f transition-f terminal-f)]
     (control exp-algorithm data)))
 
-(def tst
+(defn qs-supplier [algorithm]
   (let [data (conf 1.0 0.4 0.7)
         policy (eps-greedy 0.6 greedy-by-max)
         terminal-f (terminal? world)
         action-f actions
         transition-f (transition world terminal-f)
-        reward-f (reward world terminal-f)]
-    (lazy-traj policy action-f reward-f transition-f)))
+        reward-f (reward world terminal-f)
+        exp-algorithm (algorithm qs-channel policy action-f reward-f transition-f terminal-f)]
+    (control exp-algorithm data)))
 
 (defn block-pos
   ([x y]
@@ -53,12 +61,21 @@
   ([{x :x y :y}]
    (block-pos x y)))
 
+(defn derive-pos
+  [entity]
+  (let [x (get-in entity [:position :x])
+        y (get-in entity [:position :y])]
+    (block-pos x y)))
+
 (defn observe! [channel]
   (delayed-observer channel))
 
 (defn do-trans! [entity pair]
-  (let [pos (->> pair (:state) (:position))]
-    (-> entity (assoc :x (* (:x pos) block-size)) (assoc :y (* (:y pos) block-size)))))
+  (let [pos (block-pos (->> pair (:state) (:position)))
+        e-type (:e-type entity)]
+    (case e-type
+      :geometric (-> entity (assoc :x (+ (:x pos) half-block) :y (+ (:y pos) half-block)))
+      :texture (-> entity (assoc :x (* (:x pos) block-size) :y (* (:y pos) block-size))))))
 
 (defn move-agent [entities pair]
   (map
@@ -67,16 +84,20 @@
         (do-trans! e pair)
         e)) entities))
 
-;; FIXME: find a way to properly grade the colours based on q-values
-(defn adapt-values [entities data pr-x]
+(defn adapt-values [entities data]
   (map
-    (fn [e]
-      (let [x (:x e)
-            y (:y e)
-            values (find-some #(and
-                                (= x (get-in % [:position :x]))
-                                (= y (get-in % [:position :y]))) (:q-values data))
-            probs (->> values (pr-x) (action-mean))])) entities))
+    (fn [[S As]]
+      (let [x (-> S :position :x)
+            y (-> S :position :y)
+            lower (color :red)
+            upper (color :green)
+            mean (action-mean As)
+            n-color (color! lower :lerp upper (t mean))]
+        (assoc
+          (shape :filled
+                 :set-color n-color
+                 :rect 0 0 block-size block-size)
+          :x (* x block-size) :y (* y block-size)))) (:q-values data)))
 
 (def choice-observer (observe! policy-channel))
 (def q-values-observer (observe! qs-channel))
@@ -84,4 +105,9 @@
 (defn show-choice [entities]
   (if-let [updated (choice-observer #(move-agent entities %))]
     updated
+    entities))
+
+(defn show-qs [entities]
+  (if-let [values (q-values-observer #(adapt-values entities %))]
+    values
     entities))
